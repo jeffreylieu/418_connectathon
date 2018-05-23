c256473b37a4d9510411c8505599fd595015a1a5
Daniel S <danielksstone@gmail.com>
2018-05-23 12:29:51 -0700

    updating - no changes accidentally pressed a button while in dev

---
+++ main.js
@@ -0,0 +1,43 @@
+
+$(document).ready(connectFourStuff);
+
+function connectFourStuff() {
+    gameBoardCreate(7);
+    eventListeners();
+  }
+
+  function eventListeners() {
+
+  }
+
+  // dynamic board creator - enter the size you want for NxN
+  // by Daniel
+function gameBoardCreate(size) { // need parameter to function
+    for (var row = 0; row < size+1; row++) {
+      var rowHolder = $("<div>",{ // creating variable for new div
+        row: row, // inside each div adding row = "row-index number"
+        'class': 'row', // adding a class of row
+      })
+      for (var col = 0; col < size; col++) {
+        var column = $("<div>",{ // creating variable for each column block div
+        col: col, // inside each tag adding col = "col index number"
+        "class": 'col', // adding class of col for each column div
+      })
+        rowHolder.append(column) // appending the column to the rowHolder div
+      } // close column creation for loop
+      $("#gameBoard").append(rowHolder); // appending the rowHolder div to the gameBoard div
+    } // close row creation for loop
+  } // close gameBoardCreate function
+
+
+
+// function to change players between player one and player two
+// by Jeff
+function change( switch_button )
+{
+    if ( switch_button.value === "Player One" )
+        switch_button.value = "Player Two";
+    else
+        switch_button.value = "Player One";
+}
+
