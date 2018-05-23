2ca34b7eb35d4f6617f9cabf02c5bb6acc43ae64
Jeff Lieu <jefflieu@gmail.com>
2018-05-23 09:56:22 -0700

    Added code for button to switch between players

---
+++ main.js
@@ -0,0 +1,7 @@
+function change( switch_button )
+{
+    if ( switch_button.value === "Player One" )
+        switch_button.value = "Player Two";
+    else
+        switch_button.value = "Player One";
+}
