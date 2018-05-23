982341abdd03f376d0c2ebe8f97d267f73ed5414
sasa-us <sasa_luu@hotmail.com>
2018-05-23 16:37:11 -0700

    update clickhandler for all column & update array

--- main.js
+++ main.js
@@ -3,39 +3,47 @@ $(document).ready(connectFourStuff);
 
 function connectFourStuff() {
 
-  
+
   gameBoardCreate(7);
-  eventListeners();
+ // eventListeners();
 
 
 }
 var cell;
 
+var availableRow;
+var changedRow;
+var changedCol;
+var boardArray = [
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0'],
+                  ['0', '0', '0', '0', '0', '0', '0']
+                ];
 
-  var rows = 8;
-  var cols = 7;
-  var boardArray = [['o']];
 
-
-  // dynamic board creator - enter the size you want for NxN
-  // by Daniel
+// dynamic board creator - enter the size you want for NxN
+// by Daniel
 function gameBoardCreate(size) { // need parameter to function
-    for (var row = 0; row < size+1; row++) {
-      var rowHolder = $("<div>",{ // creating variable for new div
-        row: row, // inside each div adding row = "row-index number"
-        'class': 'row', // adding a class of row
-      })
-      for (var col = 0; col < size; col++) {
-        var column = $("<div>",{ // creating variable for each column block div
+  for (var row = 0; row < size + 1; row++) {
+    var rowHolder = $("<div>", { // creating variable for new div
+      row: row, // inside each div adding row = "row-index number"
+      'class': 'row', // adding a class of row
+    })
+    for (var col = 0; col < size; col++) {
+      var column = $("<div>", { // creating variable for each column block div
         col: col, // inside each tag adding col = "col index number"
         "class": 'col', // adding class of col for each column div
       })
-        rowHolder.append(column) // appending the column to the rowHolder div
-        column.click(handleClick);
-      } // close column creation for loop
-      $("#gameBoard").append(rowHolder); // appending the rowHolder div to the gameBoard div
-    } // close row creation for loop
-  } // close gameBoardCreate function
+      rowHolder.append(column) // appending the column to the rowHolder div
+      column.click(handleClick);
+    } // close column creation for loop
+    $("#gameBoard").append(rowHolder); // appending the rowHolder div to the gameBoard div
+  } // close row creation for loop
+} // close gameBoardCreate function
 
 
 
@@ -52,30 +60,32 @@ function change(switch_button) {
 function handleClick() {
   var currentRow = $(this).parent().attr('row');
   var currentCol = $(this).attr('col');
-  boardArray[currentRow][currentCol] = 'x';
-
-  console.log('boardArray', boardArray);
-
-  console.log('currentRow', currentRow);
+  
+  console.log('currentRow currentCol', currentRow, currentCol);
   console.log(`row: ${currentRow}, col: ${currentCol}`);
 
- 
-  // cellEmpty = false;
-  //debugger;
-  if (currentCol == 6) {
-    for (var row = 6; row > 0; row--) {
-      var cellCheck = $("div[row=" + row + "] > div[col=" + currentCol + "]");
-      console.log(cellCheck.css('background-color'))
-      
-      if (cellCheck.css('background-color') === "rgb(255, 255, 0)") {
-        $("div[row=" + row + "] > div[col=" + currentCol + "]").css('background-color', 'red');
+  availableRow = findNextRow(currentCol);
 
-        return
-      }
-
-
-    }
+  if(availableRow !== -1) {
+    fillCell(availableRow, currentCol);
+  } else {
+    alert('no space');
+    //showInvalidMove();
   }
+}//end handleClick()
 
+function fillCell(rowtoFill, coltoFill) {
+  $("div[row=" + rowtoFill + "] > div[col=" + coltoFill + "]").css('background-color', 'red');
 }
+
+function findNextRow(col) {
+  for (var row = 6; row >= 0; row--) {
+    debugger;
+    if(boardArray[row][col] === '0') {
+      console.log('in findNextRow row col is', row, col);
+      boardArray[row][col] = "1";
+      return row;
+    } 
+  }
+}//end findNextRow()
 
