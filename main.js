22490e8cab8e4ef72c4c5ee58a0e58c8534628ad
Daniel S <danielksstone@gmail.com>
2018-05-23 16:31:17 -0700

    checkwin forward slash and backslash almost complete.  saving to edit gameboard creation function

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 
 $(document).ready(connectFourStuff);
+var boardArray = [
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0']
+  ];
 
 function connectFourStuff() {
     gameBoardCreate(7);
@@ -11,6 +20,14 @@ function connectFourStuff() {
 
 
   }
+
+
+
+
+
+
+
+
 
   // dynamic board creator - enter the size you want for NxN
   // by Daniel
@@ -44,7 +61,35 @@ function change( switch_button )
 }
 
 
-var boardArray = [];
 function checkWin (boardArray) {
-
+    // check horizontal - Jeff
+    // check veritcal - Jeff
+    // check diagonal right up - Daniel
+    // check diagonal left up - Daniel
 }
+
+
+
+
+
+// //////////////////////////////////////////////////////////////// //
+var currentDrop = boardArray[currentRow,currentCol];
+
+function fourMatchChecker (currentDrop,check1,check2,check3) {
+    return (currentDrop != '0') && (currentDrop == check3) && (currentDrop == check2) && (currentDrop == check3);
+ }
+
+function diagonalForwardSlash (currentRow, currentCol) {
+    if (row < boardArray.length - 4 && col < boardArray.length - 3 &&
+        fourMatchChecker(currentDrop, boardArray[row+1][col+1], boardArray[row+2][col+2], boardArray[row+3][col+3])) {
+        return currentDrop;
+    }
+}
+
+function diagonalBackSlash (currentRow, currentCol) {
+    if (row < boardArray.length - 4 && col > 2 && 
+        fourTogether(currentDrop, boardArray[row+1][col-1], boardArray[row+2][col-2], boardArray[row+3][col-3])) {
+      return currentDrop;
+    }
+}
+
